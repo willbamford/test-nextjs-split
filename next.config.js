f46f0aded8216e52537a52a927843db5a32fdcf3
Will Bamford <will.bamford@moonpig.com>
2022-05-17 15:51:51 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    const moduleReplacement = new webpack.NormalModuleReplacementPlugin(
+      /(.*)theme-a(\.*)/,
+      function (resource) {
+        resource.request = resource.request.replace(/theme-a/, `theme-b`);
+      }
+    );
+    return {
+      ...config,
+      plugins: [...config.plugins, moduleReplacement],
+    };
+  },
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
